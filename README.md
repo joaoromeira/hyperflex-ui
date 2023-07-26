5151367ccf80e1fe40aa8def1a763e9eb4463836
João Romeira <vi.peres@icloud.com>
2023-07-26 15:53:33 -0300

    fix: fix buid and add workflows

---
+++ README.md
@@ -0,0 +1,23 @@
+# HyperFlex UI - Built on Material UI
+
+Welcome to HyperFlex UI! This is a Design System built on top of Material UI, providing a consistent collection of components, styles, and guidelines for use in our projects.
+
+## Overview
+
+HyperFlex UI is built on top of Material UI, which is a widely-used library for building responsive and user-friendly interfaces. With HyperFlex UI, you can unifying your design standards and promoting consistency across your codebase.
+
+## Getting Started
+
+To start using HyperFlex UI in your project, follow the steps below:
+
+1. Installation: Add the HyperFlex UI package to your project using package managers like npm or yarn.
+
+```bash
+npm install hyperflex-ui
+```
+
+or
+
+```bash
+yarn add my-design-system
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,35 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { resolve } from 'node:path';
+
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { EsLinter, linterPlugin } from 'vite-plugin-linter';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+import * as packageJson from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src}/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ['src/'],
+    }),
+  ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'flex-ui',
+      entry: resolve('src', 'index.ts'),
+      name: 'HyperFlex UI',
+      formats: ['es', 'umd'],
+      fileName: (format) => `hyperflex-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
     },
   },
-});
+}));
