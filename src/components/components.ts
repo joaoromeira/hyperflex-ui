51bb40c7ecbf99290babc4609d51f087e193e495
João Romeira <vi.peres@icloud.com>
2023-08-14 12:02:30 -0300

    fix: resolve exports

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,30 +1,31 @@
+export * from './box/box';
+export * from './box/box.types';
+
 export * from './button/button';
 export * from './button/button.types';
 
-export * from './tag/tag';
-export * from './tag/tag.types';
+export * from './card/card';
+export * from './card/card.types';
+
+export * from './checkbox/checkbox';
+export * from './checkbox/checkbox.types';
 
 export * from './date-picker/date-picker';
 export * from './date-picker/date-picker.types';
 
+export * from './dialog/dialog';
+export * from './dialog/dialog.types';
+
 export * from './divider/divider';
 export * from './divider/divider.types';
 
-export * from './text-field/text-field';
-export * from './text-field/text-field.types';
-
-export * from './select/select';
-export * from './select/select.types';
+export * from './form-control/form-control';
+export * from './form-control/form-control.types';
 
 export * from './grid/grid';
-export * from './stack/stack';
-export * from './text/text';
 
-export * from './tabs/tabs';
-export * from './tabs/tabs.types';
-
-export * from './dialog/dialog';
-export * from './dialog/dialog.types';
+export * from './link/link';
+export * from './link/link.types';
 
 export * from './menu-list/menu-list';
 export * from './menu-list/menu-list.types';
@@ -32,26 +33,31 @@ export * from './menu-list/menu-list.types';
 export * from './modal/modal';
 export * from './modal/modal.types';
 
-export * from './link/link';
-export * from './link/link.types';
+export * from './popover/popover';
+export * from './popover/popover.types';
 
 export * from './radio/radio';
 export * from './radio/radio.types';
 
-export * from './box/box';
-export * from './box/box.types';
+export * from './select/select';
+export * from './select/select.types';
 
-export * from './radio/radio';
-export * from './radio/radio.types';
+export * from './stack/stack';
 
 export * from './switch/switch';
 export * from './switch/switch.types';
 
+export * from './tabs/tabs';
+export * from './tabs/tabs.types';
+
+export * from './tag/tag';
+export * from './tag/tag.types';
+
+export * from './text/text';
+export * from './text/text.types';
+
+export * from './text-field/text-field';
+export * from './text-field/text-field.types';
+
 export * from './tooltip/tooltip';
 export * from './tooltip/tooltip.types';
-
-export * from './card/card';
-export * from './card/card.types';
-
-export * from './popover/popover';
-export * from './popover/popover.types';
