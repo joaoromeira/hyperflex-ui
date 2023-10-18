99008200cf0355c1804a3050ff8d017712bbfcdf
João Romeira <vi.peres@icloud.com>
2023-10-18 14:32:17 -0300

    feat: export colors token type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './foundation/theme/theme';
 export * from './components/components';
 export * from './hooks';
+export type * from './foundation/theme/tokens/tokens.types';
 
 export { breakpoints, palette, spacing, withUtility } from './foundation/utils';
